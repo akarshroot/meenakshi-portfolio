8c119f696dbe38d143f1a4b53d5f1a1194f84e0a
nerdy-monk <akarshtripathi03@gmail.com>
2023-01-26 20:41:28 +0530

    Added Collages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,40 @@
 import React, { Fragment } from 'react'
 import meenakshiImg from '../assets/meenakshi-2.png'
+import './Home.css'
 
 function Home() {
   return (
     <Fragment>
-        <div className="col py-3 d-flex align-items-center"> 
+      <div className="col py-3 d-flex align-items-center">
+        <div className='home-bg-image'>
+        <div className='home-bg-image-overlay'>
+        </div>
+        </div>
 
-            <div className="container">
+        <div className="container">
 
-              <div className="row">
+          <div className="row">
 
-                <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
-                  <div>
-                    <h1 className="m-0 font-big slide-in-top">Hi,</h1>
-                    <h1 className="m-0 font-big animate-charcter">I'm Meenakshi,</h1>
-                    <p className="ls-2 mt-2 slide-in-bottom">Computer Engineering Student / Artist / Travel Enthusiast</p>
-                  </div>
+            <div className='home-hero'>
+          <div className="row">
+
+              <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+                <div>
+                  <h1 className="m-0 font-big slide-in-top">Hi,</h1>
+                  <h1 className="m-0 font-big animate-charcter">I'm Meenakshi,</h1>
+                  <p className="ls-2 mt-2 slide-in-bottom">Computer Engineering Student / Artist / Travel Enthusiast</p>
                 </div>
-
-                <div className="col-lg-6 col-md-12 col-sm-12">
-                  <img className="img-fluid" src={meenakshiImg} alt="Meenakshi Panghal"></img>
-                </div>
-
               </div>
 
+              <div className="col-lg-6 col-md-12 col-sm-12">
+                <img className="img-fluid" src={meenakshiImg} alt="Meenakshi Panghal"></img>
+              </div>
             </div>
-            
+            </div>
+          </div>
+
         </div>
+      </div>
     </Fragment>
   )
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { SiCodechef, SiHackerrank } from 'react-icons/si';
+import { SiLeetcode, SiLinkedin } from 'react-icons/si';
 
 function Resume() {
   return (
@@ -8,7 +8,7 @@ function Resume() {
             <div className="p-4 bg-dark slide-in-top">
                 <hr></hr>
                 <div className="row">
-                    <div className="col-md-6">
+                <div className="col-md-6">
                         <h4 className="ff-jose ls-2">EDUCATION</h4>
                         <ul>
                             <li>
@@ -20,11 +20,30 @@ function Resume() {
                             <li>
                                 <h4 className="ff-jose my-1 text-red">Class 12th</h4>
                                 <h6 className="blue-label px-2 py-1">2019-2020</h6>
-                                <p className="m-0">XYZ Senior Secondary School</p>
-                                <p>Grade: 99.9%</p>
+                                <p className="m-0">Paramount Convent Senior Secondary School</p>
                             </li>
                         </ul>
 
+                    </div>
+                    <div className="col-md-3">
+                        <h4 className="ff-jose ls-2">INTERESTS</h4>
+                        <div className="row my-2">
+                                <div className='mx-2 my-3 border rounded py-1 px-2 mw-fc'>
+                                    <span>Travel</span>
+                                </div>
+                                <div className='mx-2 my-3 border rounded py-1 px-2 mw-fc'>
+                                    <span>Manual Arts</span>
+                                </div>             
+                                <div className='mx-2 my-3 border rounded py-1 px-2 mw-fc'>
+                                    <span>Yoga</span>
+                                </div>
+                                <div className='mx-2 my-3 border rounded py-1 px-2 mw-fc'>
+                                    <span>Sports</span>
+                                </div>       
+                                <div className='mx-2 my-3 border rounded py-1 px-2 mw-fc'>
+                                    <span>Winning People</span>
+                                </div>         
+                            </div>
                     </div>
                 </div>
 
@@ -34,11 +53,11 @@ function Resume() {
                     <div className="col-md-6">
                         <h4 className="ff-jose ls-2">PROFILE</h4>
                         <h1>
-                            <a href="https://www.codechef.com/users/yagnik_003" target="/blank" className="text-white text-decoration-none mx-2">
-                                <SiCodechef className="zoom-on-hover"/>
+                            <a href="https://www.linkedin.com/in/meenakshipanghal/" target="/blank" className="text-white text-decoration-none mx-2">
+                                <SiLinkedin className="zoom-on-hover"/>
                             </a> 
-                            <a href="https://www.hackerrank.com/Yagnik_003" target="/blank" className="text-white text-decoration-none mx-2">
-                                <SiHackerrank className="zoom-on-hover"/>
+                            <a href="https://leetcode.com/meenakshi/" target="/blank" className="text-white text-decoration-none mx-2">
+                                <SiLeetcode className="zoom-on-hover"/>
                             </a> 
                         </h1>
                     </div>
